e4b950a91c2376f56dfccc131a3b34ec0791c2fb
kauessilvas <kauesantosds5@gmail.com>
2025-05-10 15:40:43 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("--- Fluxograma da Engenharia ---");
+
+Console.Write("O objeto está se movendo (S/N)?: ");
+char movendo = char.ToUpper(char.Parse(Console.ReadLine()!));
+
+Console.Write("Deveria?: ");
+char deveria = char.ToUpper(char.Parse(Console.ReadLine()!));
+
+if (movendo == 'S') {
+    if (deveria == 'S') {
+        Console.WriteLine("\nÓtimo!\n");
+    } else {
+        Console.WriteLine("\nUse Silver Tape.\n");
+    }
+} else {
+    if (deveria == 'S') {
+        Console.WriteLine("\nUse WD-40.\n");
+    } else {
+        Console.WriteLine("\nÓtimo!\n");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FluxogramaDaEngenharia
 Programa que implementa o meme Fluxograma da Engenharia.
+
+![Fluxograma da Egenharia](https://github.com/diegoneri/aulas-lp-csharp/blob/master/exercises/fluxograma-engenharia.jpg?raw=true)
